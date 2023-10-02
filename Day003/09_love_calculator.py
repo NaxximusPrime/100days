f04b05fbf4d69fb253fa7f364a160574796e4e2c
Naxximus <eisteedaemon@hotmail.de>
2023-10-02 09:34:59 +0200

    added love calc and treasure challenge.

--- Day003/09_love_calculator.py
+++ Day003/09_love_calculator.py
@@ -3,14 +3,25 @@ print("You need to fill in the full name to see if it is true love.")
 name1 = input("What is your name? ").lower()
 name2 = input("What is their name? ").lower()
 
-true = name1.count("t") + name2.count("t")
-true = name1.count("r") + name2.count("r")
-true = name1.count("u") + name2.count("u")
-true = name1.count("e") + name2.count("e")
+combined_names = name1 + name2
 
-love = name1.count("l") + name2.count("l")
-love = name1.count("o") + name2.count("o")
-love = name1.count("v") + name2.count("v")
-love = name1.count("e") + name2.count("e")
 
-print(f"Your lovescore is: {true}{love}%")
+t = combined_names.count("t")
+r = combined_names.count("r")
+u = combined_names.count("u")
+e = combined_names.count("e")
+first_digit = t + r + u + e
+
+l = combined_names.count("l")
+o = combined_names.count("o")
+v = combined_names.count("v")
+e = combined_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+if (score < 10) or(score > 90):
+    print(f"fYour score is {score}%, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}%, you are alright together.")
+else:
+    print(f"Your score is {score}%.")

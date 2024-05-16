1dd9002b10a3831377bb3f625063d3ac05d11bab
NaxximusPrime <eisteedaemon@hotmail.de>
2024-05-16 08:44:08 +0200

    Code in Day002 and Day003 updated

--- Day002/01_primitive_data_types.py
+++ Day002/01_primitive_data_types.py
@@ -5,7 +5,7 @@ print("Hello"[4])  # prints the fourth character
 
 # Integer
 print(123 + 456)
-123_456_789
+print(123_456_789)
 
 # Float
 print(53.99)
--- Day002/03_exercise1.py
+++ Day002/03_exercise1.py
@@ -1,4 +1,2 @@
 numbers_sum = input("Type a two digit number: ")
 print(int(numbers_sum[0]) + int(numbers_sum[1]))
-
-
